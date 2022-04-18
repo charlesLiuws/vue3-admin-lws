63df9dcc2ccff1e24eb15b24b94a965cfb62c9b3
liuwangshan <935614816@qq.com>
2022-04-18 23:40:16 +0800

    首页布局

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus';
-import router from '@/router/index';
 import App from './App.vue'
+import router from './router/index'
+import ElementPlus from 'element-plus';
+import store from './store'
+// 初始化css 重置css默认样式
+import 'normalize.css/normalize.css'
+// 全局 css
+import '@/styles/index.scss'
 
-const app = createApp(App);
-app.use(ElementPlus);
-app.use(router);
-app.mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(ElementPlus)
+  .mount('#app')
