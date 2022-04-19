a125fa0f009d7fc53c58c36836848a2995144b45
liuwangshan <935614816@qq.com>
2022-04-19 21:47:51 +0800

    svgicon组件开发

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index'
+import router from './router/index';
 import ElementPlus from 'element-plus';
 import store from './store'
-// 初始化css 重置css默认样式
+import App from './App.vue'
 import 'normalize.css/normalize.css'
-// 全局 css
 import '@/styles/index.scss'
-
+import 'virtual:svg-icons-register'
+import initSvgIcon from '@/icons/index'
 createApp(App)
-  .use(store)
-  .use(router)
-  .use(ElementPlus)
-  .mount('#app')
+    .use(store)
+    .use(ElementPlus)
+    .use(initSvgIcon)
+    .use(router)
+    .mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path, { resolve } from 'path';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    vue(),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), 'src/icons/svg')],
+      symbolId: 'icon-[dir]-[name]',
+      inject: 'body-last',
+      customDomId: '__svg__icons__dom__'
+    })
+  ],
   resolve: {
     alias: {
-      '@': resolve(__dirname,'src')
+      '@': resolve(__dirname, 'src')
     }
-  },
-  plugins: [vue()]
+  }
 })
